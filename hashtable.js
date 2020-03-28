5904440e34602376603a0caaa5df848110663de8
陳文擎 <kevin51034@gmail.com>
2020-03-29 00:53:46 +0800

    add open addressing with linear probing

---
+++ hashtable.js
@@ -0,0 +1,150 @@
+const table = [];
+let collisionCount = 0;
+function djb2HashCode(key) {
+
+    // 初始化 hash 值，大部分實作使用 5381
+    let hash = 5381;
+    for (let i = 0; i < key.length; i++) {
+        // 根據經驗值給個魔術數字 33
+        hash = hash * 33 + key.charCodeAt(i);
+    }
+    // 1013 為隨機質數
+    return hash % 1013;
+}
+
+function put(key, value) {
+    console.log(key)
+    console.log(value)
+    let position = djb2HashCode(key);
+    // 若是位置沒被佔據直接 new 一個 ValuePair，若有則考慮下一個 index
+
+    //console.log(typeof table[position])
+    //console.log(table[position])
+
+    if (table[position] === undefined) {
+        console.log('insert')
+        table[position] = {
+            key,
+            value
+        };
+        console.log(position)
+        //console.log(table[position]);
+    } else {
+        console.log('collision')
+        console.log(position)
+        collisionCount++;
+        let index = ++position;
+        while (table[index] !== undefined) {
+            index++;
+        }
+        table[index] = {
+            key,
+            value
+        };
+        //console.log(table[index]);
+
+    }
+
+}
+
+module.exports = {
+    djb2HashCode,
+    put,
+    table,
+    collisionCount,
+    //HashTable
+}
+
+
+
+function HashTable() {
+    let table = [];
+    // 實作內部一個 ValuePair 類別，存原始 key、value  
+    let ValuePair = function (key, value) {
+        this.key = key;
+        this.value = value;
+    }
+    /*
+    let getHashTableCode = function (key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            // charCodeAt 會回傳指定字串內字元的 Unicode 編碼（可以包含中文字）
+            hash += key.charCodeAt(i);
+        }
+        // 為了取到較小值，使用任意數做除法 mod 處理
+        return hash % 37;
+    }*/
+
+    // djb2HashCode 實作
+    let djb2HashCode = function (key) {
+        // 初始化 hash 值，大部分實作使用 5381
+        let hash = 5381;
+        for (let i = 0; i < key.length; i++) {
+            // 根據經驗值給個魔術數字 33
+            hash = hash * 33 + key.charCodeAt(i);
+        }
+        // 1013 為隨機質數
+        return hash % 31;
+    }
+
+    // 由於 JavaScript 陣列可動態增加長度，所以不用擔心長度不夠問題
+    this.put = function (key, value) {
+        console.log(key)
+        console.log(value)
+        let position = djb2HashCode(key);
+        // 若是位置沒被佔據直接 new 一個 ValuePair，若有則考慮下一個 index
+        if (table[position] === undefined) {
+            table[position] = new ValuePair(key, value);
+        } else {
+            let index = ++position;
+            while (table[index] !== undefined) {
+                index++;
+            }
+            table[index] = new ValuePair(key, value);
+        }
+    }
+    this.get = function (key) {
+        let position = djb2HashCode(key);
+        // 先確認鍵值是否存在
+        if (table[position] !== undefined) {
+            // 開始比對，沒有就下一個
+            if (table[position].key === key) {
+                return table[position].value;
+            } else {
+                let index = ++position;
+                while (table[index] === undefined || table[index].key !== key) {
+                    index++;
+                }
+                if (table[index].key === key) {
+                    console.log(table[index].value);
+                    return table[index].value;
+                }
+            }
+        }
+        return undefined;
+    }
+    this.remove = function (key) {
+        let position = djb2HashCode(key);
+        // 先確認鍵值是否存在
+        if (table[position] !== undefined) {
+            // 開始比對，沒有就下一個
+            if (table[position].key === key) {
+                table[index] = undefined;
+            } else {
+                let index = ++position;
+                while (table[index] === undefined || table[index].key !== key) {
+                    index++;
+                }
+                if (table[index].key === key) {
+                    table[index] = undefined;
+                }
+            }
+        }
+        return undefined;
+    }
+    this.display = function () {
+        for (let i = 0; i < table.length; i++) {
+            console.log(table[i]);
+        }
+    }
+}
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const request = require('request');
 const fs = require('fs');
 const md5 = require('md5');
 
+var HashTable = require('./hashtable.js');
+
 //const writeStream = fs.createWriteStream('post.txt');
 
 
@@ -30,6 +32,11 @@ app.use(express.static('public'));
 
 const url = 'https://www.ptt.cc/bbs/index.html'
 //const url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+//const table = new Int32Array(100);
+//const table = [];
+//var table = new HashTable();
+
+//let seenhashTable = new HashTable();
 
 request({
     url,
@@ -62,6 +69,7 @@ request({
     // select all link
     let link = [];
     let linkmd5 = [];
+    let hashKey = [];
     $('a').each(function (i, elem) {
         if ($(this).attr('href')) {
             link.push($(this).attr('href'));
@@ -70,11 +78,21 @@ request({
             link.push(thisurl);
             //console.log(md5(thisurl));
             linkmd5.push(md5(thisurl));
+            //hashKey.push(djb2Hash(md5(thisurl)))
+            //console.log(djb2Hash(md5(thisurl)));
+            //console.log(djb2Hash(md5(thisurl)) % (64 * 256 * 2 - 1));
+            //hashKeymod = djb2Hash(md5(thisurl)) % (64 * 256 * 2 - 1);
+            //seenDB[hashKeymod] = md5(thisurl);
+            HashTable.put(md5(thisurl), thisurl);
+
         }
 
 
     })
     console.log(link)
+    //console.log(table)
+    //seenhashTable.display();
+    //console.log(table.length)
     fs.writeFile('link.txt', `${link}`, function (err) {
         if (err)
             console.log(err);
@@ -82,6 +100,18 @@ request({
             console.log('Write operation complete.');
     });
     fs.writeFile('linkmd5.txt', `${linkmd5}`, function (err) {
+        if (err)
+            console.log(err);
+        else
+            console.log('Write operation complete.');
+    });
+    fs.writeFile('djb2HashKey.txt', `${hashKey}`, function (err) {
+        if (err)
+            console.log(err);
+        else
+            console.log('Write operation complete.');
+    });
+    fs.writeFile('seenhashTable.txt', `${HashTable.table}`, function (err) {
         if (err)
             console.log(err);
         else
@@ -102,6 +132,154 @@ request({
             console.log('Write operation complete.');
     });
 })
+/*
+function djb2Hash(str) {
+    var len = str.length;
+    var hash = 5381;
+    for (var idx = 0; idx < len; ++idx) {
+        hash = 33 * hash + str.charCodeAt(idx);
+    }
+    return hash;
+}
+
+function ValuePair(key, value) {
+    this.key = key;
+    this.value = value;
+}*/
+/*
+function djb2HashCode(key) {
+    // 初始化 hash 值，大部分實作使用 5381
+    let hash = 5381;
+    for (let i = 0; i < key.length; i++) {
+        // 根據經驗值給個魔術數字 33
+        hash = hash * 33 + key.charCodeAt(i);
+    }
+    // 1013 為隨機質數
+    return hash % 1013;
+}
+
+function put(key, value) {
+    console.log('put')
+    console.log(key)
+    console.log(value)
+    let position = djb2HashCode(key);
+    // 若是位置沒被佔據直接 new 一個 ValuePair，若有則考慮下一個 index
+
+    console.log(typeof table[position])
+    console.log(table[position])
+
+    if (table[position] === undefined) {
+        console.log('insert')
+        table[position] = {key, value};
+        console.log(position)
+        //console.log(table[position]);
+    } else {
+        console.log('collision')
+        console.log(position)
+
+        let index = ++position;
+        while (table[index] !== undefined) {
+            index++;
+        }
+        table[index] = {key, value};
+        //console.log(table[index]);
+
+    }
+    
+}
+
+function HashTable() {
+    //let table = [];
+    // 實作內部一個 ValuePair 類別，存原始 key、value  
+    let ValuePair = function (key, value) {
+        this.key = key;
+        this.value = value;
+    }
+    /*
+    let getHashTableCode = function (key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            // charCodeAt 會回傳指定字串內字元的 Unicode 編碼（可以包含中文字）
+            hash += key.charCodeAt(i);
+        }
+        // 為了取到較小值，使用任意數做除法 mod 處理
+        return hash % 37;
+    }
+
+    // djb2HashCode 實作
+    let djb2HashCode = function (key) {
+        // 初始化 hash 值，大部分實作使用 5381
+        let hash = 5381;
+        for (let i = 0; i < key.length; i++) {
+            // 根據經驗值給個魔術數字 33
+            hash = hash * 33 + key.charCodeAt(i);
+        }
+        // 1013 為隨機質數
+        return hash % 31;
+    }
+
+    // 由於 JavaScript 陣列可動態增加長度，所以不用擔心長度不夠問題
+    this.put = function (key, value) {
+        console.log(key)
+        console.log(value)
+        let position = djb2HashCode(key);
+        // 若是位置沒被佔據直接 new 一個 ValuePair，若有則考慮下一個 index
+        if (table[position] === undefined) {
+            table[position] = new ValuePair(key, value);
+        } else {
+            let index = ++position;
+            while (table[index] !== undefined) {
+                index++;
+            }
+            table[index] = new ValuePair(key, value);
+        }
+    }
+    this.get = function (key) {
+        let position = djb2HashCode(key);
+        // 先確認鍵值是否存在
+        if (table[position] !== undefined) {
+            // 開始比對，沒有就下一個
+            if (table[position].key === key) {
+                return table[position].value;
+            } else {
+                let index = ++position;
+                while (table[index] === undefined || table[index].key !== key) {
+                    index++;
+                }
+                if (table[index].key === key) {
+                    console.log(table[index].value);
+                    return table[index].value;
+                }
+            }
+        }
+        return undefined;
+    }
+    this.remove = function (key) {
+        let position = djb2HashCode(key);
+        // 先確認鍵值是否存在
+        if (table[position] !== undefined) {
+            // 開始比對，沒有就下一個
+            if (table[position].key === key) {
+                table[index] = undefined;
+            } else {
+                let index = ++position;
+                while (table[index] === undefined || table[index].key !== key) {
+                    index++;
+                }
+                if (table[index].key === key) {
+                    table[index] = undefined;
+                }
+            }
+        }
+        return undefined;
+    }
+    this.display = function(){
+        for(let i=0;i<table.length;i++){
+            console.log(table[i]);
+        }
+    }
+}*/
+
 
 
 
